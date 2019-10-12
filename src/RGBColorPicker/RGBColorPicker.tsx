de21aeb6226afa2aac391ce7c519b5ca676985d0
Sarfaraj Shahjahan <dee.tech.au@gmail.com>
2019-10-12 19:58:12 +0200

    add sampleStyle to SingleColorPicker and RGBColorPicker, added state to RGBColorPicker

--- src/RGBColorPicker/RGBColorPicker.tsx
+++ src/RGBColorPicker/RGBColorPicker.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import SingleColorPicker, {baseColor} from './SingleColorPicker'; 
 interface IProps{};
-interface IState{};
+interface IState{
+    rValue:number;
+    gValue:number;
+    bValue:number;
+};
 
 export default class RGBColorPicker extends React.PureComponent<IProps, IState> {
 
     constructor(props: IProps) {
       console.log("new App component will be initialized");
       super(props);
+      this.state = {
+          rValue:5,
+          gValue:50,
+          bValue:200
+      }
     }
   
     render(){
+        let colorSample: string = `rgb(${this.state.rValue},${this.state.gValue},${this.state.bValue})`
+        const rgbStyle = {
+            display: "inline-block",
+            width: "20px",
+            height: "20px",
+            "background-color": colorSample
+        }
       return <div>
-        <SingleColorPicker color={baseColor.r} intensity={5}/>
-        <SingleColorPicker color={baseColor.g} intensity={100}/>
-        <SingleColorPicker color={baseColor.b} intensity={200} />
-        <p>mixed color</p>
+        <SingleColorPicker color={baseColor.r} intensity={this.state.rValue}/>
+        <SingleColorPicker color={baseColor.g} intensity={this.state.gValue}/>
+        <SingleColorPicker color={baseColor.b} intensity={this.state.bValue} />
+        <div style={rgbStyle}></div>mixed color
       </div>
     }
   }
--- src/RGBColorPicker/SingleColorPicker.tsx
+++ src/RGBColorPicker/SingleColorPicker.tsx
@@ -2,34 +2,52 @@ import React from 'react';
 import { JSXAttribute } from '@babel/types';
 
 export enum baseColor {
-    r="red",
-    g="green",
-    b="blue"
+    r = "red",
+    g = "green",
+    b = "blue"
 }
-interface IProps{
-   color:baseColor
-   intensity:number
+interface IProps {
+    color: baseColor
+    intensity: number
 };
 
-interface IState{};
+interface IState { };
 
 export default class SingleColorPicker extends React.PureComponent<IProps, IState> {
 
     constructor(props: IProps) {
-      console.log("new App component will be initialized");
-      super(props);
+        console.log("new App component will be initialized");
+        super(props);
     }
-  
-    render(){
+
+    render() {
         const scpStyle = {
-            display:"inline-block",
-            width:"20px",
-            height:"20px",
-            "background-color":this.props.color
-       }
-      return <div>
-          <div style={scpStyle}></div><input type="number" value = {this.props.intensity}/>
-       I'm a ColorPicker for {this.props.color}
-      </div>
+            display: "inline-block",
+            width: "20px",
+            height: "20px",
+            "background-color": this.props.color
+        }
+        let colorSample: string = "";
+        switch (this.props.color) {
+            case baseColor.r:
+                colorSample = `rgb(${this.props.intensity},0,0)`;
+                break;
+            case baseColor.g:
+                colorSample = `rgb(0,${this.props.intensity},0)`;
+                break;
+            case baseColor.b:
+                colorSample = `rgb(0,0,${this.props.intensity})`;
+                break;
+        }
+        const sampleStyle = {
+            display: "inline-block",
+            width: "20px",
+            height: "20px",
+            "background-color": colorSample
+        }
+        return <div>
+            <div style={scpStyle}></div><div style={sampleStyle}></div><input type="number" value={this.props.intensity} />
+            I'm a ColorPicker for {this.props.color}
+        </div>
     }
-  }
+}
